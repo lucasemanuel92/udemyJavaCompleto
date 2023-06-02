2f19c9c65215a1f76de8c4fd9a8c37ffe77ed0b0
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2023-06-02 12:06:52 -0300

    Add files via upload

---
+++ fundamentos/aulas/Primeiro_programa.java
@@ -0,0 +1,13 @@
+/*
+ * AUTOR: Lucas  Emanuel Oliveira de Carvalho
+ * PROJETO: Criar um programa para printar um texto qualquer
+ */
+
+package fundamentos;
+
+public class Primeiro_programa {
+	public static void main(String[] args) {
+		System.out.println("Primeiro texto");
+	}
+
+}
