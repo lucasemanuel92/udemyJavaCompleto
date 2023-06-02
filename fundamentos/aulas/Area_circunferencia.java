4dc197b7e854aa5ce85e5514045f1714da5ea8c3
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2023-06-02 12:30:22 -0300

    Add files via upload

---
+++ fundamentos/aulas/Area_circunferencia.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class Area_circunferencia {
+
+	public static void main(String[] args) {
+		double raio = 3;
+		double pi = 3.14;
+		
+		double area = raio* pi;
+		
+		System.out.println(area);
+		
+
+	}
+
+}
